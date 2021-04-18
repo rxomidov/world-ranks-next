238ff9ff0b5e945fcb19f1fc180784951093036f
@rixsitilloxomidov <rixsitilloxomidov@gmail.com>
2021-04-18 17:17:48 +0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+// import React from "react";
+import styles from '../styles/Home.module.css'
+import Layout from "../components/Layout/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+        main
+    </Layout>
+  )
+}
